73e1b93193d759e51f16f430625af2dd626c0841
edsonwf <edsonwf@hotmail.com>
2025-08-18 15:41:35 -0300

    cria uma entidade no banco de dados apartir do ClienteModel

--- src/main/java/dev/edson/Cadastro/ClienteController.java
+++ src/main/java/dev/edson/Cadastro/ClienteController.java
@@ -1,13 +1,12 @@
 package dev.edson.Cadastro;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class ClienteController {
 
     @GetMapping("/boasVindas")
     public String boasVindas(){
